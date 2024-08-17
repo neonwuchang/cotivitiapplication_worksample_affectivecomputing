bd3dff59802f54a6d1d3abf1dce00f82432ad70b
neonwuchang <103783716+neonwuchang@users.noreply.github.com>
2024-08-17 15:55:20 -0700

    Update and rename emotion-measurement_stream.py to PoC_demo-code/emotion-measurement_stream.py
    Added source links for material used to run demo tests

--- PoC_demo-code/emotion-measurement_stream.py
+++ PoC_demo-code/emotion-measurement_stream.py
@@ -9,7 +9,7 @@ HUME_API_KEY=os.getenv("HUME_API_KEY")
 client = HumeStreamClient(HUME_API_KEY)
 
 # text sample for analysis
-# source: 
+# transcriot from initial part of video, source link: https://www.youtube.com/shorts/5yNCBwx91uQ and title: "Stranded Passenger frustrated over tech outage"
 samples = [
     "I'm so upset right now!",
     "There are eight people in my party. Eight people! $456 a ticket, and they're giving me a $100 back! That is it!"
@@ -44,10 +44,12 @@ print("TEXT STREAM:- ")
 asyncio.run(txt_stream())
 print("\n\n-------------------------------------------------------------------------------------------------------------------\n\n")
 print("VID/ AUD:- ")
-emotions=asyncio.run(audvid("Downloads/jlaw.mp4"))
+# clip used for analysis from source title "Jennifer Lawrence's reaction to every wing on Hot Ones" and source link: https://www.youtube.com/shorts/1ntwS9EbaQ8 
+emotions=asyncio.run(audvid("")) # add path here, removed for uploading on github
 print("ALL EMOTIONS AND SCORES: ")
 print(emotions)
 print("------------------------------------------------------------------------------------------------------------------------")
-emotions = sorted(emotions, key=lambda d: d['score'])
+# extracting top 10
+emotions = sorted(emotions, key=lambda d: d['score']) # sort by score assigned to each emotion 
 print("TOP 10 ONLY: ")
 print(emotions[-1:-11:-1])
